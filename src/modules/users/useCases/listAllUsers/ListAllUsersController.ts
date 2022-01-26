08e466f5dd26b2ad4b0ff5800a301249aeec561e
Besutti <marcelobesutti@gmail.com>
2022-01-25 22:16:04 -0300

    agora com swagger

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,13 @@ class ListAllUsersController {
     constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
     handle(request: Request, response: Response): Response {
-        const { user_id } = request.headers;
+        // pensando um pouco melhor quandoq que não tem parametro cai no showUserProfile!
+        // const { user_id } = request.headers;
 
         try {
             const all = this.listAllUsersUseCase.execute({
-                user_id: String(user_id),
+                // user_id: String(user_id),
+                user_id: "",
             });
 
             return response.json(all);
